36e46660f34a8727e4979d88ef0df6f2762c2761
IOSEdwin <eramirezn23@icloud.com>
2025-02-28 09:52:20 +0100

    sdo comentario , pendiente muchas cosas, esto lo hago por si se elimna al clonar el de mansour

---
+++ app/src/main/java/com/example/zodiac/HoroscopeAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.zodiac
+
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class HoroscopeAdapter:Adapter {
+
+
+
+}
+
+class HoroscopeViewHolder(view:View):ViewHolder (view){
+
+}
--- app/src/main/java/com/example/zodiac/MainActivity.kt
+++ app/src/main/java/com/example/zodiac/MainActivity.kt
@@ -7,6 +7,24 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val horoscopeList = listOf(
+        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "taurus",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "gemini",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "cancer",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "leo",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+//        Horoscope(id = "aries",R.drawable.aries_icon,R.string.horoscope_name_aries,R.string.horoscope_date_aries),
+
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
